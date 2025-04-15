437046b805027796b29d0b7277258bdd4d43fd70
Matias <lit.io30303@gmail.com>
2025-04-15 16:04:16 -0400

    fix backend

--- backend-ecommerce/config/server.ts
+++ backend-ecommerce/config/server.ts
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  const port = env.int('PORT', 1337); // Predeterminado: 1337
+  console.log(`Servidor iniciado en el puerto: ${port}`);
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port,
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
